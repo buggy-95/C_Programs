ce1e423eb30eaeea79d8f2d92cd8bbee63c54a7a
Qin <850307035@qq.com>
2016-11-29 12:25:46 +0800

    要交的作业

--- 12-5.c
+++ 12-5.c
@@ -34,24 +34,24 @@ typedef struct Students {
     char email[30];
 } ss;
 
+int iseof(FILE *);
 void menue();
 void message_input();
 void show();
 ss seek();//姓名\学号查找
 void sort();//姓名\学号\生日排序
 void revise();//修改\删除
-
+void find();
 
 int main()
 {
-    message_input();
+    // message_input();
     show();
     return 0;
 }
 
 void message_input() {
-    FILE *fp;
-    fp = fopen("students.txt", "a");
+    FILE *fp = fopen("students.txt", "a");
     if(!fp)
     {
         printf("errror!\n");
@@ -88,27 +88,103 @@ void message_input() {
     fclose(fp);
 }
 
-void show() { //显示数据的列表
-    ss student;
-    FILE *fp = fopen("students.txt", "r");
-    fread(&student, sizeof(student), 1, fp);
+void show() {
     printf("┏━━━━━┳━━━┳━━┳━━┳━━━━━┳━━━━━━┓\n");
     printf("┃   学号   ┃ 姓名 ┃性别┃年龄┃ 出生日期 ┃    电话    ┃\n");
     printf("┣━━━━━╋━━━╋━━╋━━╋━━━━━╋━━━━━━┫\n");
-    // printf("┃%10s┃%-6s┃ %2s ┃ %2d ┃%4d-%02d-%02d┃%11s ┃\n", student.number, student.name, student.sex, student.age, student.birth.year, student.birth.month, student.birth.day, student.phone);
-    printf("┃");
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-    printf("%10s", student.number);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    printf("┃");
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
-    printf("%-6s", student.name);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
-    printf("┃ %2s ┃ %2d ┃%4d-%02d-%02d┃%11s ┃\n", student.sex, student.age, student.birth.year, student.birth.month, student.birth.day, student.phone);
-    printf("┣━━━┳━┻━━━┻━━┻━━┻━━━━━┻━━━━━━┫\n");
-    printf("┃地址: ┃%-48s┃\n", student.address);
-    printf("┣━━━╋━━━━━━━━━━━━━━━━━━━━━━━━┫\n");
-    printf("┃邮件: ┃%-48s┃\n", student.email);
+    ss student;
+    FILE *fp = fopen("students.txt", "r");
+    fread(&student, sizeof(student), 1, fp);
+    while(!feof(fp)){
+        printf("┃");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+        printf("%10s", student.number);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+        printf("┃");
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+        printf("%-6s", student.name);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+        printf("┃ %2s ┃ %2d ┃%4d-%02d-%02d┃%11s ┃\n", student.sex, student.age, student.birth.year, student.birth.month, student.birth.day, student.phone);
+        // printf("┃%10s┃%-6s┃ %2s ┃ %2d ┃%4d-%02d-%02d┃%11s ┃\n", student.number, student.name, student.sex, student.age, student.birth.year, student.birth.month, student.birth.day, student.phone);
+        printf("┣━━━┳━┻━━━┻━━┻━━┻━━━━━┻━━━━━━┫\n");
+        printf("┃地址: ┃%-48s┃\n", student.address);
+        printf("┣━━━╋━━━━━━━━━━━━━━━━━━━━━━━━┫\n");
+        printf("┃邮件: ┃%-48s┃\n", student.email);
+        fread(&student, sizeof(student), 1, fp);
+        if(!feof(fp))
+            printf("┣━━━┻━┳━━━┳━━┳━━┳━━━━━┳━━━━━━┫\n");
+    }
     printf("┗━━━┻━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
+    fclose(fp);
+}
+int iseof(FILE *fp) {
+    fseek(fp, 1L, 1);
+    if(feof(fp)) {
+        fseek(fp, -1L, 1);
+        return 1;
+    } else {
+        fseek(fp, -1L, 1);
+        return 0;
+    }
+}
+void find() {
+    char findstr[20];
+    int choose, find = 0;
+    ss student;
+    do {
+        printf("1) 按学号查找；\n");
+        printf("2) 按姓名查找；\n");
+        printf("0) 退出。\n");
+        do {
+            scanf("%d", &choose);
+            fflush(stdin);
+            if(choose == 0) {
+                break;
+            } else if(choose == 1) {
+                printf("请输入学号：");
+            } else if(choose == 2) {
+                printf("请输入姓名：");
+            } else {
+                printf("输入错误，请重新输入：");
+            }
+        } while(choose < 0 || choose > 2);
+        scanf("%s", findstr);
+        fflush(stdin);
+        
+        char* student_info = (choose == 1) ? student.number : student.name;
+
+    } while(choose > 0 && choose <= 2);
+
+
+    // scanf("%s", number);
+    FILE *fp = fopen("students.txt", "r");
+    while(!feof(fp)) {
+        fread(&student, sizeof(student), 1, fp);
+        if(0 == strcmp(student.number, findstr)) {
+            fseek(fp, -sizeof(student), 1);
+            printf("┏━━━━━┳━━━┳━━┳━━┳━━━━━┳━━━━━━┓\n");
+            printf("┃   学号   ┃ 姓名 ┃性别┃年龄┃ 出生日期 ┃    电话    ┃\n");
+            printf("┣━━━━━╋━━━╋━━╋━━╋━━━━━╋━━━━━━┫\n");
+            printf("┃");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
+            printf("%10s", student.number);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+            printf("┃");
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12);
+            printf("%-6s", student.name);
+            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 11);
+            printf("┃ %2s ┃ %2d ┃%4d-%02d-%02d┃%11s ┃\n", student.sex, student.age, student.birth.year, student.birth.month, student.birth.day, student.phone);
+            printf("┣━━━┳━┻━━━┻━━┻━━┻━━━━━┻━━━━━━┫\n");
+            printf("┃地址: ┃%-48s┃\n", student.address);
+            printf("┣━━━╋━━━━━━━━━━━━━━━━━━━━━━━━┫\n");
+            printf("┃邮件: ┃%-48s┃\n", student.email);
+            printf("┗━━━┻━━━━━━━━━━━━━━━━━━━━━━━━┛\n");
+            find = 1;
+            break;
+        }
+        // printf("running\n");
+    }
+    if(!find)
+        printf("未找到\n");
     fclose(fp);
 }
